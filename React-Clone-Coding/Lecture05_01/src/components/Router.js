1382821929d116eea4d65570558831a062b424ae
BYSooooo <avera0128@gmail.com>
2023-04-21 23:29:05 +0900

    Nomad Clone Coding Lecture

---
+++ React-Clone-Coding/Lecture05_01/src/components/Router.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
+
+const AppRouter = ({isLoggedIn, userObj}) => {
+    return (
+        <Router>
+            {isLoggedIn && <Navigation userObj={userObj}/>}
+            <Routes>
+                {isLoggedIn ? 
+                <>
+                    <Route exact path="/" element={<Home userObj={userObj}/>}/>
+                    <Route exact path="/profile" element={<Profile userObj={userObj}/>} />
+                </> : 
+                <>
+                    <Route exact path="/" element={<Auth />} />
+                </>}
+            </Routes>
+        </Router>
+    )
+}
+export default AppRouter;
