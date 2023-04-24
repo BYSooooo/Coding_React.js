a089a2fd79f02c2fa20659c22ce4ede5b57c81c1
BYSooooo <avera0128@gmail.com>
2023-04-24 23:32:06 +0900

    Nomad Clone Coding Lecture

---
+++ React-Clone-Coding/Lecture06_01/src/routes/Auth.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { authService } from "fbase";
+import {  GithubAuthProvider, 
+          GoogleAuthProvider, 
+          signInWithPopup} from "firebase/auth";    
+import AuthForm from "components/AuthForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faGoogle, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+const Auth = () => {
+  const onSocialClick = async (event) => {
+    const {
+      target : { name }
+    } = event;
+    let provider;
+    if(name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+    console.log(data)
+  }
+  
+  return (
+    <div className="authContainer">
+      <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="3x" style={{ marginBottom : 30}}/>
+      <AuthForm />
+      <div className="authBtns">  
+        <button onClick={onSocialClick} name="google" className="authBtn">
+          Cotinue with Google <FontAwesomeIcon icon={faGoogle} />
+        </button>
+        <button onClick={onSocialClick} name="github" className="authBtn">
+          Cotinue with Github <FontAwesomeIcon icon={faGithub} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
