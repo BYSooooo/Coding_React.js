b6291f54bed4270617b9ab3957e7bf8294008491
BYSooooo <avera0128@gmail.com>
2023-05-10 00:27:16 +0900

    Nomad Redux Lecture

--- Redux-for-Beginners/Lecture04_01/src/store.js
+++ Redux-for-Beginners/Lecture04_01/src/store.js
@@ -4,31 +4,24 @@ import { createAction } from "@reduxjs/toolkit";
 const ADD = "ADD";
 const DELETE = "DELETE"
 
-// const addToDo = (text) => {
-//     return {
-//         type : ADD,
-//         id : Date.now(), 
-//         text
-//     }
-// }
-
-// const deleteToDo = id => {
-//     return {
-//         type : DELETE,
-//         id : parseInt(id)
-//     }
-// }
-
-const addToDo = createAction("ADD")
+const addToDo = createAction("ADD", function prepare(text) {
+    return {
+        payload : {
+            text,
+            id : Date.now()
+        }
+    }
+})
 const deleteToDo = createAction("DELETE")
 
 const reducer = (state = [], action) => {
     switch (action.type) {
         case addToDo.type : 
             console.log(action)
-            return [{ text : action.payload, id: action.id}, ...state];
+            return [{ text : action.payload.text, id: action.payload.id}, ...state];
         case deleteToDo.type : 
-            return state.filter(toDo => toDo.id !== action.id);
+            console.log(action)
+            return state.filter(toDo => toDo.id !== action.payload);
         default :
             return state;
     }
