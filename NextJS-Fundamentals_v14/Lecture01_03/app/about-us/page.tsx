f13a7d4cf7afe769d5236ca2ffe899c261c29ee0
BYSooooo <avera0128@gmail.com>
2024-06-04 23:09:12 +0900

    [240604]Nomad Corder Lecture

---
+++ NextJS-Fundamentals_v14/Lecture01_03/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import Navigation from "../../components/navigation"
+
+export default function AboutUs() {
+    return (
+        <div>
+            <Navigation />
+            <h1>
+                About us!
+            </h1>
+        </div>
+    ) 
+}
