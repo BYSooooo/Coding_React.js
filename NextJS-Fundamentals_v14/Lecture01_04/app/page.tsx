702428839fa94d3565e7e4de7cbd918197d87bb1
BYSooooo <avera0128@gmail.com>
2024-06-05 23:54:16 +0900

    [240605]Nomad Coder Lecture

---
+++ NextJS-Fundamentals_v14/Lecture01_04/app/page.tsx
@@ -0,0 +1,12 @@
+import Navigation from "../components/navigation";
+
+export default function Page() {
+    return (
+        <div>
+            <Navigation />
+            <h1>
+                Hello!
+            </h1>
+        </div>
+    )
+}
