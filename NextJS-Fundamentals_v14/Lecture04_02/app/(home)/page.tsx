8bb402de6d5fc66fddbd081133501b9c17199cac
BYSooooo <avera0128@gmail.com>
2024-06-19 00:29:12 +0900

    [240619]Some Modified

--- NextJS-Fundamentals_v14/Lecture04_02/app/(home)/page.tsx
+++ NextJS-Fundamentals_v14/Lecture04_02/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
     title : "Home"
@@ -15,40 +16,10 @@ async function getMovies() {
 export default async function HomePage() {
     const movies = await getMovies()
     return (
-        <div>
-            {movies.map((movie) => {
+        <div className={styles.container}>
+            {movies.map((movie) => 
                 <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
-            })}
+            )}
         </div>
     )
 }
-//"use client";
-    
-
-/** Client Side Fetching Code */
-
-// import { useEffect, useState } from "react"
-
-// export default function Page() {
-
-//     const [isLoading, setIsLoading] = useState(true);
-//     const [movies, setMovies] = useState();
-    
-//     const getMovies = async()=> {
-//         const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
-//         const json = await response.json()
-//         setMovies(json)
-//         setIsLoading(false)
-//     }
-
-//     useEffect(()=> {
-//         getMovies()
-//     },[])
-
-//     return (
-//         <div>
-            
-//             {isLoading ? "Loading" : JSON.stringify(movies)}
-//         </div>
-//     )
-// }
